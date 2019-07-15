691b41fc4fc59e1c4b9d64382f07b56941367828
Milica Ninković <milica.ninkovic95@gmail.com>
2019-07-16 00:29:20 +0200

    Adding button handlers

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import {pressKey} from '../actions/actions';
+import { connect } from 'react-redux';
 
 class Button extends Component {
     constructor(props) {
@@ -10,32 +12,38 @@ class Button extends Component {
 
     }
 
-    // handleClick = e => {
-    //     e.target
-    // }
+    handleClick = e => {
+        console.log('event triggered!');
+        pressKey({displayStatus: '', displayValue: this.state.value});
+    };
+
     render(){
        if(this.state.symbol === 'A' || this.state.symbol === 'B') {
           return (
-            <div className="keyboard__button">
+            <div className="keyboard__button" onClick={this.handleClick}>
                 <p className="text--large align--center">{this.state.value}</p>
                 <p className="keyboard__symbol align--right">{this.state.symbol}</p>
             </div>
           )
        } else if (this.state.symbol !== '') {
           return (
-            <div className="keyboard__button">
+            <div className="keyboard__button" onClick={this.handleClick}>
                 <p className="text--large align--center">{this.state.value}</p>
                 <p className="keyboard__symbol align--right"><i className={"fas fa-long-arrow-alt-"+ this.state.symbol}></i></p>
             </div>
           )
        } else {
           return (
-            <div className="keyboard__button">
+            <div className="keyboard__button" onClick={this.handleClick}>
                 <p className="text--large align--center">{this.state.value}</p>
             </div>
           )
        }
     }
 }
+const mapStateToProps = (state) => state;
 
-export default Button;
+const mapDispatchToProps = {
+    ...pressKey
+};
+export default connect(mapStateToProps,mapDispatchToProps)(Button);
