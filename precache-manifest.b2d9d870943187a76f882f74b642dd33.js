0125d103953e2255d6768c6cd2cff1d037a11a1e
Milica Ninković <milica.ninkovic95@gmail.com>
2019-07-18 16:06:24 +0200

    Updates

---
+++ precache-manifest.b2d9d870943187a76f882f74b642dd33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "357a81c55b706dbfd31b7c905e59207d",
+    "url": "/deposit-box/index.html"
+  },
+  {
+    "revision": "d4a315520009482ed6dd",
+    "url": "/deposit-box/static/css/main.786fdd46.chunk.css"
+  },
+  {
+    "revision": "d8019fa0f1624cc0371f",
+    "url": "/deposit-box/static/js/2.632498a0.chunk.js"
+  },
+  {
+    "revision": "d4a315520009482ed6dd",
+    "url": "/deposit-box/static/js/main.2ee1a61b.chunk.js"
+  },
+  {
+    "revision": "e4146bd47591717c50bd",
+    "url": "/deposit-box/static/js/runtime~main.b9062fa1.js"
+  }
+]);
--- src/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import displayReducer from "./reducers/displayReducer";
-import validationReducer from "./reducers/validationReducer";
-import START_TIMER from 'redux-timer-middleware';
-
-const timer =  START_TIMER;
-export default createStore(combineReducers({displayReducer, validationReducer}),{},applyMiddleware(timer));
